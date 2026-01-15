102993c484c064740c130016ea12980406d45748
stellariumImpl <fifa404yi@yahoo.com>
2026-01-15 22:44:16 +0800

    feat: Implement Prisma ORM with initial User and Post models, database migration, and client integration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <Button variant="outline">Click me</Button>
+      {JSON.stringify(users)}
     </div>
   );
 };
