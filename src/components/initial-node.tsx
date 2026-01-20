92520ea3fb37502b28992448dc338dfc25777db8
stellariumImpl <fifa404yi@yahoo.com>
2026-01-20 13:51:33 +0800

    16: Node Selector

--- src/components/initial-node.tsx
+++ src/components/initial-node.tsx
@@ -2,23 +2,27 @@
 
 import type { NodeProps } from "@xyflow/react";
 import { PlusIcon } from "lucide-react";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { PlaceholderNode } from "./react-flow/placeholder-node";
 import { WorkflowNode } from "./workflow-node";
+import { NodeSelector } from "./node-selector";
 
 export const InitialNode = memo((props: NodeProps) => {
+  const [selectorOpen, setSelectorOpen] = useState(false);
   return (
-    <WorkflowNode
-      // name="Initial node"
-      // description="Click to add a node"
-      showToolbar={false}
-    >
-      <PlaceholderNode {...props} onClick={() => {}}>
-        <div className="cursor-pointer flex items-center justify-center">
-          <PlusIcon className="size-4" />
-        </div>
-      </PlaceholderNode>
-    </WorkflowNode>
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+      <WorkflowNode
+        // name="Initial node"
+        // description="Click to add a node"
+        showToolbar={false}
+      >
+        <PlaceholderNode {...props} onClick={() => setSelectorOpen(true)}>
+          <div className="cursor-pointer flex items-center justify-center">
+            <PlusIcon className="size-4" />
+          </div>
+        </PlaceholderNode>
+      </WorkflowNode>
+    </NodeSelector>
   );
 });
 
---
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -0,0 +1,20 @@
+import { NodeProps } from "@xyflow/react";
+import { memo } from "react";
+
+import { BaseTriggerNode } from "../base-trigger-node";
+import { MousePointerIcon } from "lucide-react";
+
+export const ManualTriggerNode = memo((props: NodeProps) => {
+  return (
+    <>
+      <BaseTriggerNode
+        {...props}
+        icon={MousePointerIcon}
+        name="When clicking on 'Execute workflow'"
+        // status={nodeStatus} // TODO
+        // onSettings={handleOpenSettings} // TODO
+        // onDoubleClick={handleOpenSettings} // TODO
+      />
+    </>
+  );
+});
