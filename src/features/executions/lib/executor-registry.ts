ec3a66c12c966ec8110d3dd0dc48c849113c211c
stellariumImpl <fifa404yi@yahoo.com>
2026-01-22 20:21:38 +0800

    not very sure about that

--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -3,13 +3,13 @@ import { NodeExecutor } from "../types";
 import { manualTriggerExecutor } from "@/features/triggers/components/manual-trigger/executor";
 import { httpRequestDataExecutor } from "../components/http-request/executor";
 
-export const executorRegistry: Record<NodeType, NodeExecutor> = {
+export const executorRegistry: Record<NodeType, NodeExecutor<any>> = {
   [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
   [NodeType.INITIAL]: manualTriggerExecutor,
-  [NodeType.HTTP_REQUEST]: httpRequestDataExecutor, // TODO: fix types
+  [NodeType.HTTP_REQUEST]: httpRequestDataExecutor,
 };
 
-export const getExecutor = (type: NodeType): NodeExecutor => {
+export const getExecutor = (type: NodeType): NodeExecutor<any> => {
   const executor = executorRegistry[type];
   if (!executor) {
     throw new Error(`No executor found for node type ${type}`);
